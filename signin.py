911270299eeda0e69f38333dad1345238e0fa964
gudezhi <gudezhi@gmail.com>
2021-11-23 09:17:40 +0800

    采用cookie登陆

--- signin.py
+++ signin.py
@@ -5,7 +5,7 @@ import json
 
 # hao4k 账户信息
 username = os.environ["HAO4K_USERNAME"]
-password = os.environ["HAO4K_PASSWORD"]
+saltkey = os.environ["HAO4K_SALTKEY"]
 # 企微通知
 corpid = os.environ["corpid"]
 corpsecret = os.environ["corpsecret"]
@@ -13,42 +13,38 @@ corpsecret = os.environ["corpsecret"]
 # hao4k 签到 url
 user_url = "https://www.hao4k.cn//member.php?mod=logging&action=login"
 base_url = "https://www.hao4k.cn/"
-signin_url = "https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&formhash={formhash}&format=empty"
-form_data = {
-    'formhash': "",
-    'referer': "https://www.hao4k.cn/",
-    'username': username,
-    'password': password,
-    'questionid': "0",
-    'answer': ""
-}
-inajax = '&inajax=1'
+signin_url = "https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&formhash={formhash}&format=empty&inajax=1&ajaxtarget=JD_sign"
+
 
 def run(form_data):
     s = requests.Session()
-    s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'})
-    headers = {"Content-Type": "text/html", 'Connection': 'close'}
-    user_resp = s.get(user_url,headers=headers)
-    login_text = re.findall('action="(.*?)"', user_resp.text)
-    for loginhash in login_text:
-        if 'loginhash' in loginhash:
-            login_url = base_url + loginhash + inajax
-            login_url = login_url.replace("amp;", "")
-            print(login_url)
-    form_text = re.search('formhash=(.*?)\'', user_resp.text)
-    print(form_text.group(1))
-    form_data['formhash'] = form_text.group(1)
-    print(form_data)
+    headers = {
+        'authority': 'www.hao4k.cn',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'dnt': '1',
+        'x-requested-with': 'XMLHttpRequest',
+        'sec-ch-ua-mobile': '?0',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+        'sec-ch-ua-platform': '"macOS"',
+        'accept': '*/*',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.hao4k.cn/qiandao/',
+        'accept-language': 'en-US,en;q=0.9',
+        'cookie': 'Hm_lvt_6c1cdfd1254d8b1be9c00f4cdfe0c69e=1637551360; HxHg_2132_noticeTitle=1; HxHg_2132_saltkey='+saltkey+'; HxHg_2132_lastvisit=1637547949; HxHg_2132_sendmail=1; HxHg_2132_isms_login_referer=https%3A%2F%2Fwww.hao4k.cn%2Fqiandao%2F; HxHg_2132_seccodeSsDzt=52539.737feacd1c14a59ad6; HxHg_2132_sid=g1CzF9; HxHg_2132_ulastactivity=54859%2BOB6ZR1CwlTdVde6YtmAqOOUoiQTgEio3%2FHmznxgzkR7xn5; HxHg_2132_auth=e01ea2Sj7h7Lqiu54MAdutyWVjV9tzW9ZqiZGzoeYYdNbOOqIc8%2BWZMJU4B2gj%2F%2FpLPoDFcPhwEYYFd%2BEJvkFlusfD4; HxHg_2132_lastcheckfeed=761023%7C1637551596; HxHg_2132_checkfollow=1; HxHg_2132_lip=58.33.129.194%2C1637551358; HxHg_2132_connect_is_bind=0; HxHg_2132_myrepeat_rr=R0; HxHg_2132_curcountl=0; HxHg_2132_checkpm=1; Hm_lpvt_6c1cdfd1254d8b1be9c00f4cdfe0c69e=1637551597; HxHg_2132_lastact=1637551596%09plugin.php%09',
+    }
 
-    login_resp = s.post(login_url, data=form_data)
-    test_resp = s.get('https://www.hao4k.cn/k_misign-sign.html',headers=headers)
+    test_resp = s.get('https://www.hao4k.cn/qiandao/',headers=headers)
+    # print(test_resp.text)
     if username in test_resp.text:
       print('login!')
     else:
       return 'login failed!'
     signin_text = re.search('formhash=(.*?)"', test_resp.text)
-    signin_resp = s.get(signin_url.format(formhash=signin_text.group(1)))
-    test_resp = s.get('https://www.hao4k.cn/k_misign-sign.html',headers=headers)
+    signin_resp = s.get(signin_url.format(formhash=signin_text.group(1)),headers=headers)
+    print(signin_resp.text)
+    test_resp = s.get('https://www.hao4k.cn/qiandao/',headers=headers)
     if '您的签到排名' in test_resp.text:
       print('signin!')
     else:
@@ -82,7 +78,8 @@ def sendMsg(data):
     print(resp.json())
 
 if __name__ == "__main__":
-  signin_log = run(form_data)
+  # signin_log = run(form_data)
+  signin_log = run({})
   if signin_log is None:
     send_content = "hao4k 每日签到成功！"
     print('Sign in automatically!')
